735240b59aa0230b1b3fc98cbe5d88d466b27ed4
Sebascoga22 <sebascoga2030@gmail.com>
2024-12-05 12:01:58 -0400

    feat: se agrego la clase para grafos no dirigidos

--- GrafoListaAdyecente.cpp
+++ GrafoListaAdyecente.cpp
@@ -10,21 +10,21 @@ struct Node {
     Node* next;
 };
 
-// Clase para representar un grafo
+// Clase para representar un grafo dirigido
 template <typename T>
-class graph {
+class DirectedGraph {
     T V; // Número de vértices
     Node<T>** adjList; // Arreglo de listas de adyacencia
 public:
-    graph(int V); // Constructor del grafo
+    DirectedGraph(int V); // Constructor del grafo
     void addEdge(T v, T w); // Función para agregar una arista
-    void printGraph(); // Función para imprimir el grafo
-    ~graph(); // Destructor del grafo para ahorra memoria
+    void PrintDirectedGraph(); // Función para imprimir el grafo
+    ~DirectedGraph(); // Destructor del grafo para ahorra memoria
 };
 
 // Constructor del grafo
 template <typename T>
-graph<T>::graph(int V) {
+DirectedGraph<T>::DirectedGraph(int V) {
     this->V = V;
     // Se inicializa el array de listas de adyacencia
     adjList = new Node<T>*[V];
@@ -32,19 +32,19 @@ graph<T>::graph(int V) {
         adjList[i] = NULL; // Se inicializa vacia
     }
 
-// Funcion para agregar una arista
+// Funcion para agregar una arista al grafo dirigido
 template <typename T>
-void graph<T>::addEdge(T v, T w) {
+void DirectedGraph<T>::addEdge(T v, T w) {
     Node<T>* newNode = new Node<T>{w, adjList[v]};
     adjList[v] = newNode;
 }
 
-// Funcion para imprimir el grafo
+// Funcion para imprimir el grafo dirigido
 template <typename T>
-void graph<T>::printGraph() {
+void DirectedGraph<T>::printGraph() {
     for (int v = 0; v < V; ++v) {
         Node<T>* temp = adjList[v];
-        cout << "\n Lista de adyacencia del vértice " << v << "\n";
+        cout << "\n Lista de adyacencia del vértice: " << v << "\n";
         while (temp) {
             cout << " -> " << temp->vertex; // Se imprime el vertice adyacente
             temp = temp->next;
@@ -52,9 +52,68 @@ void graph<T>::printGraph() {
     }
 }
 
-// Destructor del grafo para ahorra memoria
+// Destructor del grafo dirigido para ahorra memoria 
 template <typename T>
-graph<T>::~graph() {
+DirectedGraph<T>::~DirectedGraph() {
+    for (int i = 0; i < V; i++) {
+        Node<T>* temp = adjList[i];
+        while (temp) {
+            Node<T>* toDelete = temp;
+            temp = temp->next;
+            delete toDelete;
+        }
+    }
+    delete[] adjList;
+}
+
+// Clase para representar un grafo no dirigido
+template <typename T>
+class UndirectedGraph {
+    T V; // Número de vértices
+    Node<T>** adjList; // Arreglo de listas de adyacencia
+public:
+    UndirectedGraph(int V); // Constructor del grafo
+    void AddUndirectedEdge(T v, T w); // Función para agregar una arista
+    void PrintUndirectedGraph(); // Función para imprimir el grafo
+    ~UndirectedGraph(); // Destructor del grafo para ahorra memoria
+};
+
+// Constructor del grafo no dirigido
+template <typename T>
+UndirectedGraph<T>::UndirectedGraph(int V) {
+    this->V = V;
+    // Se inicializa el array de listas de adyacencia
+    adjList = new Node<T>*[V];
+    for (int i = 0; i < V; i++) 
+        adjList[i] = NULL; // Se inicializa vacia
+    }
+
+// Funcion para agregar una arista al grafo no dirigido
+template <typename T>
+void UndirectedGraph<T>::AddUndirectedEdge(T v, T w) {
+    Node<T>* newNode = new Node<T>{w, adjList[v]};
+    adjList[v] = newNode;
+    // Añade v a la lista de w del grafo no dirigido
+    newNode = new Node<T>{v, adjList[w]};
+    adjList[w] = newNode;
+}
+
+// Funcion para imprimir el grafo no dirigido
+template <typename T>
+void UndirectedGraph<T>::PrintUndirectedGraph() {
+    for (int v = 0; v < V; ++v) {
+        Node<T>* temp = adjList[v];
+        cout << "\n Lista de adyacencia del vértice: " << v << "\n";
+        while (temp) {
+            cout << " -> " << temp->vertex; // Se imprime el vertice adyacente
+            temp = temp->next;
+        }
+    }
+}
+
+// Destructor del grafo dirigido para ahorra memoria 
+template <typename T>
+UndirectedGraph<T>::~UndirectedGraph() {
     for (int i = 0; i < V; i++) {
         Node<T>* temp = adjList[i];
         while (temp) {
@@ -71,8 +130,11 @@ int main() {
     // Configurar la consola para usar UTF-8
     SetConsoleOutputCP(CP_UTF8); // Establece la salida en UTF-8, caracteres especiales en español
     SetConsoleCP(CP_UTF8);       // Establece la entrada en UTF-8, caracteres especiales en español
-
-    graph<int> g(5);
+    
+    cout<<"Grafo dirigido: \n";
+    
+    // Output para el grafo dirigido
+    DirectedGraph<int> g(5);
     g.addEdge(0, 1);
     g.addEdge(0, 4);
     g.addEdge(1, 2);
@@ -81,6 +143,23 @@ int main() {
     g.addEdge(2, 3);
     g.addEdge(3, 4);
     // Imprimir el grafo
-    g.printGraph();
+    g.PrintDirectedGraph();
+    
+    cout<<"\n";
+
+    cout<<"Grafo no dirigido: \n";
+    
+    // Output para el grafo no dirigido
+    UndirectedGraph<int> h(5);
+    h.AddUndirectedEdge(0, 1);
+    h.AddUndirectedEdge(0, 4);
+    h.AddUndirectedEdge(1, 2);
+    h.AddUndirectedEdge(1, 3);
+    h.AddUndirectedEdge(1, 4);
+    h.AddUndirectedEdge(2, 3);
+    h.AddUndirectedEdge(3, 4);
+    // Imprimir el grafo
+    h.PrintUndirectedGraph();
+
     return 0;
 }
--- GrafoMatriz.cpp
+++ GrafoMatriz.cpp
@@ -1,21 +1,21 @@
 #include <iostream> 
 using namespace std;
 
-// Clase para representar un grafo
+// Clase para representar un grafo dirigido
 template <typename T>
-class graph {
-    int V; // Número de vértices
+class DirectedGraph {
+    T V; // Número de vértices
     T **adjMatrix; // Puntero a la matriz de adyacencia
 public:
-    graph(int V); // Constructor del grafo
+    DirectedGraph(int V); // Constructor del grafo
     void addEdge(int i, int j); // Función para agregar una arista
-    void printGraph(); // Función para imprimir el grafo
-    ~graph(); // Destructor del grafo para ahorra memoria
+    void PrintDirectedGraph(); // Función para imprimir el grafo
+    ~DirectedGraph(); // Destructor del grafo para ahorra memoria
 };
 
-// Constructor
+// Constructor del grafo dirigido
 template <typename T>
-graph<T>::graph(int V) {
+DirectedGraph<T>::DirectedGraph(int V) {
     this->V = V;
     // Se crea la matriz de adyacencia
     adjMatrix = new T*[V];
@@ -26,16 +26,15 @@ graph<T>::graph(int V) {
     }
 }
 
-// Funcion para agregar una arista al grafo
+// Funcion para agregar una arista al grafo dirigido
 template <typename T>
-void graph<T>::addEdge(int i, int j) {
+void DirectedGraph<T>::addEdge(int i, int j) {
     adjMatrix[i][j] = 1; // i a j
-    adjMatrix[j][i] = 1; // j a i
 }
 
-// Funcion para imprimir el grafo
+// Funcion para imprimir el grafo dirigido
 template <typename T>
-void graph<T>::printGraph() {
+void DirectedGraph<T>::PrintDirectedGraph() {
     for (int i = 0; i < V; i++) {
         cout << i << " : ";
         for (int j = 0; j < V; j++) 
@@ -44,17 +43,70 @@ void graph<T>::printGraph() {
     }
 }
 
-// Destructor para el grafo
+// Destructor para el grafo dirigido
 template <typename T>
-graph<T>::~graph() {
+DirectedGraph<T>::~DirectedGraph() {
     for (int i = 0; i < V; i++) 
         delete[] adjMatrix[i];
     delete[] adjMatrix;
 }
 
-// Función principal
+// Clase para representar un grafo dirigido
+template <typename T>
+class UndirectedGraph {
+    T V; // Número de vértices
+    T **adjMatrix; // Puntero a la matriz de adyacencia
+public:
+    UndirectedGraph(int V); // Constructor del grafo
+    void AddUndirectedEdge(int i, int j); // Función para agregar una arista
+    void PrintUndirectedGraph(); // Función para imprimir el grafo
+    ~UndirectedGraph(); // Destructor del grafo para ahorra memoria
+};
+
+// Constructor del grafo no dirigido
+template <typename T>
+UndirectedGraph<T>::UndirectedGraph(int V) {
+    this->V = V;
+    // Se crea la matriz de adyacencia
+    adjMatrix = new T*[V];
+    for (int i = 0; i < V; i++) {
+        adjMatrix[i] = new T[V];
+        for (int j = 0; j < V; j++) 
+            adjMatrix[i][j] = 0; // Se inicializa el array vacio
+    }
+}
+
+// Funcion para agregar una arista al grafo no dirigido
+template <typename T>
+void UndirectedGraph<T>::AddUndirectedEdge(int i, int j) {
+    adjMatrix[i][j] = 1; // i a j
+    adjMatrix[j][i] = 1; // j a i en grafo no dirigido
+}
+
+// Funcion para imprimir el grafo dirigido
+template <typename T>
+void UndirectedGraph<T>::PrintUndirectedGraph() {
+    for (int i = 0; i < V; i++) {
+        cout << i << " : ";
+        for (int j = 0; j < V; j++) 
+            cout << adjMatrix[i][j] << " "; // Se imprime la matriz de adyacencia
+        cout << "\n"; // Se imprime un salto de línea para separar los vértices
+    }
+}
+
+// Destructor para el grafo no dirigido
+template <typename T>
+UndirectedGraph<T>::~UndirectedGraph() {
+    for (int i = 0; i < V; i++) 
+        delete[] adjMatrix[i];
+    delete[] adjMatrix;
+}
+
 int main() {
-    graph<int> g(5);
+
+    cout<<"Grafo dirigido: \n";
+    // Output para el grafo dirigido
+    DirectedGraph<int> g(5);
     g.addEdge(0, 1);
     g.addEdge(0, 4);
     g.addEdge(1, 2);
@@ -63,6 +115,23 @@ int main() {
     g.addEdge(2, 3);
     g.addEdge(3, 4);
     // Imprimir el grafo
-    g.printGraph();
+    g.PrintDirectedGraph();
+    
+    cout<<"\n";
+
+    cout<<"Grafo no dirigido: \n";
+    
+    // Output para el grafo no dirigido
+    UndirectedGraph<int> h(5);
+    h.AddUndirectedEdge(0, 1);
+    h.AddUndirectedEdge(0, 4);
+    h.AddUndirectedEdge(1, 2);    
+    h.AddUndirectedEdge(1, 3);
+    h.AddUndirectedEdge(1, 4);    
+    h.AddUndirectedEdge(2, 3);
+    h.AddUndirectedEdge(3, 4);
+    // Imprimir
+    h.PrintUndirectedGraph();
+
     return 0;
 }
