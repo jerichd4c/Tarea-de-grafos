0b0ca789f7db1b1f0c02792d59afb168270f1052
Sebascoga22 <sebascoga2030@gmail.com>
2024-12-05 09:28:58 -0400

    fix: se corrigio los codigos no trabajando con templates

--- GrafoListaAdyecente.cpp
+++ GrafoListaAdyecente.cpp
@@ -4,56 +4,61 @@
 using namespace std;
 
 // Estructura del nodo por lista de adyacencia
+template <typename T>
 struct Node {
-    int vertex;
+    T vertex;
     Node* next;
 };
 
 // Clase para representar un grafo
+template <typename T>
 class graph {
-    int V; // Número de vértices
-    Node* *adjList; // Arreglo de listas de adyacencia
+    T V; // Número de vértices
+    Node<T>** adjList; // Arreglo de listas de adyacencia
 public:
     graph(int V); // Constructor del grafo
-    void addEdge(int v, int w); // Función para agregar una arista
+    void addEdge(T v, T w); // Función para agregar una arista
     void printGraph(); // Función para imprimir el grafo
     ~graph(); // Destructor del grafo para ahorra memoria
 };
 
 // Constructor del grafo
-graph::graph(int V) {
+template <typename T>
+graph<T>::graph(int V) {
     this->V = V;
     // Se inicializa el array de listas de adyacencia
-    adjList = new Node*[V];
+    adjList = new Node<T>*[V];
     for (int i = 0; i < V; i++) 
         adjList[i] = NULL; // Se inicializa vacia
     }
 
 // Funcion para agregar una arista
-void graph::addEdge(int v, int w) {
-    Node* newNode = new Node{w, adjList[v]};
+template <typename T>
+void graph<T>::addEdge(T v, T w) {
+    Node<T>* newNode = new Node<T>{w, adjList[v]};
     adjList[v] = newNode;
 }
 
 // Funcion para imprimir el grafo
-void graph::printGraph() {
+template <typename T>
+void graph<T>::printGraph() {
     for (int v = 0; v < V; ++v) {
-        Node* temp = adjList[v];
+        Node<T>* temp = adjList[v];
         cout << "\n Lista de adyacencia del vértice " << v << "\n";
         while (temp) {
             cout << " -> " << temp->vertex; // Se imprime el vertice adyacente
             temp = temp->next;
         }
-        cout << "\n";
     }
 }
 
 // Destructor del grafo para ahorra memoria
-graph::~graph() {
+template <typename T>
+graph<T>::~graph() {
     for (int i = 0; i < V; i++) {
-        Node* temp = adjList[i];
+        Node<T>* temp = adjList[i];
         while (temp) {
-            Node* toDelete = temp;
+            Node<T>* toDelete = temp;
             temp = temp->next;
             delete toDelete;
         }
@@ -67,7 +72,7 @@ int main() {
     SetConsoleOutputCP(CP_UTF8); // Establece la salida en UTF-8, caracteres especiales en español
     SetConsoleCP(CP_UTF8);       // Establece la entrada en UTF-8, caracteres especiales en español
 
-    graph g(5);
+    graph<int> g(5);
     g.addEdge(0, 1);
     g.addEdge(0, 4);
     g.addEdge(1, 2);
--- GrafoMatriz.cpp
+++ GrafoMatriz.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 // Clase para representar un grafo
+template <typename T>
 class graph {
     int V; // Número de vértices
-    int **adjMatrix; // Puntero a la matriz de adyacencia
+    T **adjMatrix; // Puntero a la matriz de adyacencia
 public:
     graph(int V); // Constructor del grafo
     void addEdge(int i, int j); // Función para agregar una arista
@@ -13,36 +14,39 @@ public:
 };
 
 // Constructor
-graph::graph(int V) {
+template <typename T>
+graph<T>::graph(int V) {
     this->V = V;
     // Se crea la matriz de adyacencia
-    adjMatrix = new int*[V];
+    adjMatrix = new T*[V];
     for (int i = 0; i < V; i++) {
-        adjMatrix[i] = new int[V];
-        for (int j = 0; j < V; j++) {
+        adjMatrix[i] = new T[V];
+        for (int j = 0; j < V; j++) 
             adjMatrix[i][j] = 0; // Se inicializa el array vacio
-        }
     }
 }
 
 // Funcion para agregar una arista al grafo
-void graph::addEdge(int i, int j) {
+template <typename T>
+void graph<T>::addEdge(int i, int j) {
     adjMatrix[i][j] = 1; // i a j
     adjMatrix[j][i] = 1; // j a i
 }
 
 // Funcion para imprimir el grafo
-void graph::printGraph() {
+template <typename T>
+void graph<T>::printGraph() {
     for (int i = 0; i < V; i++) {
         cout << i << " : ";
         for (int j = 0; j < V; j++) 
             cout << adjMatrix[i][j] << " "; // Se imprime la matriz de adyacencia
-        cout << "\n"; // Se imprime un salto de línea para separar los vérticesendl;
+        cout << "\n"; // Se imprime un salto de línea para separar los vértices
     }
 }
 
 // Destructor para el grafo
-graph::~graph() {
+template <typename T>
+graph<T>::~graph() {
     for (int i = 0; i < V; i++) 
         delete[] adjMatrix[i];
     delete[] adjMatrix;
@@ -50,7 +54,7 @@ graph::~graph() {
 
 // Función principal
 int main() {
-    graph g(5);
+    graph<int> g(5);
     g.addEdge(0, 1);
     g.addEdge(0, 4);
     g.addEdge(1, 2);
